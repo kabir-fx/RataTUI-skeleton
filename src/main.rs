7c82be2d5dd6446c2c7c55896376acdb66e7fe0f
kabir-fx <vatshivam49888@gmail.com>
2026-02-26 20:00:54 +0530

    feat: Implement multi-threaded event handling using mpsc channels for user input and background progress updates.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{io, sync::mpsc, thread, time::Duration};
 
 use crossterm::event::{KeyCode, KeyEventKind};
 use ratatui::{
@@ -18,10 +18,26 @@ fn main() -> io::Result<()> {
     let mut app = App {
         exit_flag: false,
         progess_bar_colour: Color::Green,
+        background_progress: 0_f64,
     };
 
+    // Create the transmitter and receiver for our application
+    let (custom_event_tx, custom_event_rx) = mpsc::channel::<CustomEvents>();
+
+    // Use a cloned tx as our main thread to receive the key events from the user
+    let tx_to_input_events = custom_event_tx.clone();
+    thread::spawn(move || {
+        handle_input_events(tx_to_input_events);
+    });
+
+    // Use the original tx to delegate the tasks of background process on a new thread
+    let tx_to_background_process = custom_event_tx;
+    thread::spawn(move || {
+        handle_background_process(tx_to_background_process);
+    });
+
     // Run the app instance
-    let app_result = app.run(&mut terminal);
+    let app_result = app.run(&mut terminal, custom_event_rx);
 
     // Restore the terminal state by exiting raw mode
     ratatui::restore();
@@ -30,20 +46,58 @@ fn main() -> io::Result<()> {
     app_result
 }
 
+// ENUM to define all the events supported by our application
+enum CustomEvents {
+    Input(crossterm::event::KeyEvent),
+    Progress(f64),
+}
+
+/// Functionality offered by input handling thread
+///
+/// Args:
+/// - We wil require some form of transmitter to send the events to our main thread
+fn handle_input_events(tx: mpsc::Sender<CustomEvents>) {
+    loop {
+        match crossterm::event::read().unwrap() {
+            crossterm::event::Event::Key(key_event) => {
+                tx.send(CustomEvents::Input(key_event)).unwrap()
+            }
+            _ => {}
+        }
+    }
+}
+
+fn handle_background_process(tx: mpsc::Sender<CustomEvents>) {
+    let mut progress = 0_f64;
+    let increment = 0.01_f64;
+
+    loop {
+        thread::sleep(Duration::from_millis(100));
+        progress += increment;
+        progress = progress.min(1_f64);
+        tx.send(CustomEvents::Progress(progress)).unwrap();
+    }
+}
+
 struct App {
     exit_flag: bool,
     progess_bar_colour: Color,
+    background_progress: f64,
 }
 
 impl App {
     /// Function responsible for running an app instance
-    fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+    fn run(
+        &mut self,
+        terminal: &mut DefaultTerminal,
+        rx: mpsc::Receiver<CustomEvents>,
+    ) -> io::Result<()> {
         // Endless loop that re-renders until we receive the exit flag
         while !self.exit_flag {
             // We need to read the keys clicked by the user to handle the exit from our application
-            match crossterm::event::read()? {
-                crossterm::event::Event::Key(key_event) => self.handle_key_event(key_event)?,
-                _ => {}
+            match rx.recv().unwrap() {
+                CustomEvents::Input(key_event) => self.handle_key_event(key_event)?,
+                CustomEvents::Progress(cur_progress) => self.background_progress = cur_progress,
             }
 
             terminal.draw(|frame| self.draw(frame))?;
@@ -82,7 +136,7 @@ impl Widget for &App {
         let [top_area, gauge_area] = vertical_layout.areas(area);
 
         // Render a title on top of the layout
-        Line::from("Hello, World!").bold().render(top_area, buf);
+        Line::from("Process Overview").bold().centered().render(top_area, buf);
 
         // Render instruction texts
         let instructions = Line::from(vec![
@@ -104,8 +158,11 @@ impl Widget for &App {
         let progress_bar = Gauge::default()
             .gauge_style(Style::default().fg(self.progess_bar_colour))
             .block(block)
-            .label(format!("Process 1: 50%"))
-            .ratio(0.5);
+            .label(format!(
+                "Process 1: {:.2}%",
+                self.background_progress * 100_f64
+            ))
+            .ratio(self.background_progress);
 
         progress_bar.render(
             Rect {
